f0c501b64515ffe2953848eb9475890e88aa6621
hnirt <nt@winged.info>
2018-04-13 14:15:08 +0100

    Renamed class to JSON2

--- json2.js
+++ json2.js
@@ -151,9 +151,7 @@
 // Create a JSON object only if one does not already exist. We create the
 // methods in a closure to avoid creating global variables.
 
-if (typeof JSON !== "object") {
-    JSON = {};
-}
+JSON2 = {};
 
 (function () {
     "use strict";
@@ -360,17 +358,17 @@ if (typeof JSON !== "object") {
 
 // If the JSON object does not yet have a stringify method, give it one.
 
-    if (typeof JSON.stringify !== "function") {
-        meta = {    // table of character substitutions
-            "\b": "\\b",
-            "\t": "\\t",
-            "\n": "\\n",
-            "\f": "\\f",
-            "\r": "\\r",
-            "\"": "\\\"",
-            "\\": "\\\\"
-        };
-        JSON.stringify = function (value, replacer, space) {
+    
+    meta = {    // table of character substitutions
+        "\b": "\\b",
+        "\t": "\\t",
+        "\n": "\\n",
+        "\f": "\\f",
+        "\r": "\\r",
+        "\"": "\\\"",
+        "\\": "\\\\"
+    };
+    JSON2.stringify = function (value, replacer, space) {
 
 // The stringify method takes a value and an optional replacer, and an optional
 // space parameter, and returns a JSON text. The replacer can be a function
@@ -378,88 +376,86 @@ if (typeof JSON !== "object") {
 // A default replacer method can be provided. Use of the space parameter can
 // produce text that is more easily readable.
 
-            var i;
-            gap = "";
-            indent = "";
+        var i;
+        gap = "";
+        indent = "";
 
 // If the space parameter is a number, make an indent string containing that
 // many spaces.
 
-            if (typeof space === "number") {
-                for (i = 0; i < space; i += 1) {
-                    indent += " ";
-                }
+        if (typeof space === "number") {
+            for (i = 0; i < space; i += 1) {
+                indent += " ";
+            }
 
 // If the space parameter is a string, it will be used as the indent string.
 
-            } else if (typeof space === "string") {
-                indent = space;
-            }
+        } else if (typeof space === "string") {
+            indent = space;
+        }
 
 // If there is a replacer, it must be a function or an array.
 // Otherwise, throw an error.
 
-            rep = replacer;
-            if (replacer && typeof replacer !== "function" &&
-                    (typeof replacer !== "object" ||
-                    typeof replacer.length !== "number")) {
-                throw new Error("JSON.stringify");
-            }
+        rep = replacer;
+        if (replacer && typeof replacer !== "function" &&
+                (typeof replacer !== "object" ||
+                typeof replacer.length !== "number")) {
+            throw new Error("JSON.stringify");
+        }
 
 // Make a fake root object containing our value under the key of "".
 // Return the result of stringifying the value.
 
-            return str("", {"": value});
-        };
-    }
+        return str("", {"": value});
+    };
 
 
 // If the JSON object does not yet have a parse method, give it one.
 
-    if (typeof JSON.parse !== "function") {
-        JSON.parse = function (text, reviver) {
+    JSON2.parse = function (text, reviver) {
 
 // The parse method takes a text and an optional reviver function, and returns
 // a JavaScript value if the text is a valid JSON text.
 
-            var j;
+        var j;
 
-            function walk(holder, key) {
+        function walk(holder, key) {
 
 // The walk method is used to recursively walk the resulting structure so
 // that modifications can be made.
 
-                var k;
-                var v;
-                var value = holder[key];
-                if (value && typeof value === "object") {
-                    for (k in value) {
-                        if (Object.prototype.hasOwnProperty.call(value, k)) {
-                            v = walk(value, k);
-                            if (v !== undefined) {
-                                value[k] = v;
-                            } else {
-                                delete value[k];
-                            }
+            var k;
+            var v;
+            var value = holder[key];
+            if (value && typeof value === "object") {
+                for (k in value) {
+                    if (Object.prototype.hasOwnProperty.call(value, k)) {
+                        v = walk(value, k);
+                        if (v !== undefined) {
+                            value[k] = v;
+                        } else {
+                            delete value[k];
                         }
                     }
                 }
-                return reviver.call(holder, key, value);
             }
+            return reviver.call(holder, key, value);
+        }
 
 
 // Parsing happens in four stages. In the first stage, we replace certain
 // Unicode characters with escape sequences. JavaScript handles many characters
 // incorrectly, either silently deleting them, or treating them as line endings.
 
-            text = String(text);
-            rx_dangerous.lastIndex = 0;
-            if (rx_dangerous.test(text)) {
-                text = text.replace(rx_dangerous, function (a) {
-                    return "\\u" +
-                            ("0000" + a.charCodeAt(0).toString(16)).slice(-4);
-                });
-            }
+        text = String(text);
+        rx_dangerous.lastIndex = 0;
+        if (rx_dangerous.test(text)) {
+            text = text.replace(rx_dangerous, function (a) {
+                return "\\u" +
+                        ("0000" + a.charCodeAt(0).toString(16)).slice(-4);
+            });
+        }
 
 // In the second stage, we run the text against regular expressions that look
 // for non-JSON patterns. We are especially concerned with "()" and "new"
@@ -474,33 +470,32 @@ if (typeof JSON !== "object") {
 // we look to see that the remaining characters are only whitespace or "]" or
 // "," or ":" or "{" or "}". If that is so, then the text is safe for eval.
 
-            if (
-                rx_one.test(
-                    text
-                        .replace(rx_two, "@")
-                        .replace(rx_three, "]")
-                        .replace(rx_four, "")
-                )
-            ) {
+        if (
+            rx_one.test(
+                text
+                    .replace(rx_two, "@")
+                    .replace(rx_three, "]")
+                    .replace(rx_four, "")
+            )
+        ) {
 
 // In the third stage we use the eval function to compile the text into a
 // JavaScript structure. The "{" operator is subject to a syntactic ambiguity
 // in JavaScript: it can begin a block or an object literal. We wrap the text
 // in parens to eliminate the ambiguity.
 
-                j = eval("(" + text + ")");
+            j = eval("(" + text + ")");
 
 // In the optional fourth stage, we recursively walk the new structure, passing
 // each name/value pair to a reviver function for possible transformation.
 
-                return (typeof reviver === "function")
-                    ? walk({"": j}, "")
-                    : j;
-            }
+            return (typeof reviver === "function")
+                ? walk({"": j}, "")
+                : j;
+        }
 
 // If the text is not JSON parseable, then a SyntaxError is thrown.
 
-            throw new SyntaxError("JSON.parse");
-        };
-    }
+        throw new SyntaxError("JSON.parse");
+    };
 }());
